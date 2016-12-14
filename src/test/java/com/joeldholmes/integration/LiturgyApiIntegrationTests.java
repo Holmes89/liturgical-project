7d1bbe1a4303845abec6a77418a5611f7b9edf58
Joel Holmes <holmes89@gmail.com>
2016-12-14 09:09:00 -0500

    integration tests

--- src/test/java/com/joeldholmes/integration/LiturgyApiIntegrationTests.java
+++ src/test/java/com/joeldholmes/integration/LiturgyApiIntegrationTests.java
@@ -1,39 +1,52 @@
 package com.joeldholmes.integration;
 
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasSize;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.restassured.RestAssured;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class LiturgyApiIntegrationTests {
 	
-	@Value("${local.server.port}")
-    protected int port;
+	@LocalServerPort
+	int port;
 
     @Before
     public final void before() {
+
     	RestAssured.port = port;
+//    	RestAssured.baseURI = "http://localhost";
+    	RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+    
+    @Test
+    public void testConnection(){
+    	get("/resourcesInfo").then().statusCode(200);
     }
     
     @Test
     public void testLiturgyNoInput(){
     	get("/api/liturgy/").
-    	then().body("data", empty());
+    	then()
+    	.statusCode(200)
+    	.body("data", empty());
     }
     
     @Test
     public void testLiturgDate(){
     	get("/api/liturgy/?filter[date]=2017-06-04").
-    	then().body("data", hasSize(1));
+    	then()
+    	.statusCode(200)
+    	.body("data", hasSize(1));
     }
 }
