34437b31fc424b6955fcedb75325dfcb7da56fe1
Joel Holmes <holmes89@gmail.com>
2016-12-20 13:33:39 -0500

    spring rest doc

--- src/test/java/com/joeldholmes/integration/LiturgyApiIntegrationTests.java
+++ src/test/java/com/joeldholmes/integration/LiturgyApiIntegrationTests.java
@@ -1,48 +1,68 @@
 package com.joeldholmes.integration;
 
-import static io.restassured.RestAssured.get;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
+import static com.jayway.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.restassured.operation.preprocess.RestAssuredPreprocessors.*;
+
 
 import org.joda.time.DateTime;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.jayway.restassured.response.*;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.builder.RequestSpecBuilder;
+import com.jayway.restassured.specification.RequestSpecification;
+
 import io.katharsis.resource.registry.ResourceRegistry;
-import io.restassured.RestAssured;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class LiturgyApiIntegrationTests {
+	
+	@Rule
+	public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
 	
 	@LocalServerPort
 	int port;
 	
 	@Autowired
 	ResourceRegistry registry;
+	
+	private RequestSpecification spec;
 
     @Before
     public final void before() {
-
     	RestAssured.port = port;
-    	RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    	this.spec = new RequestSpecBuilder()
+				.addFilter(documentationConfiguration(restDocumentation)).build();
     }
+
     
     @Test
     public void testConnection(){
-    	get("/resourcesInfo").then().statusCode(200);
+    	given(this.spec)
+   // 	.accept("application/json") 
+		.filter(document("resources"))
+		.get("/resourcesInfo").then().statusCode(200);
     }
     
     @Test
     public void testLiturgyNoInput(){
-    	get("/api/liturgy/").
+    	given(this.spec)
+  //  	.accept("application/json") 
+		.filter(document("emptyInput"))
+		.get("/api/liturgy/").
     	then()
     	.statusCode(200)
     	.body("data", empty());
@@ -50,7 +70,11 @@ public class LiturgyApiIntegrationTests {
     
     @Test
     public void testLiturgDate(){
-    	get("/api/liturgy/?filter[date]=2017-06-04").
+    	given(this.spec)
+  //  	.accept("application/json") 
+		.filter(document("index", preprocessRequest(prettyPrint(), modifyUris().host("liturgy.jholmestech.com").port(8080)), 
+				preprocessResponse(prettyPrint()))) 
+		.get("/api/liturgy/?filter[date]=2017-06-04").
     	then()
     	.statusCode(200)
     	.body("data", hasSize(1))
@@ -59,7 +83,10 @@ public class LiturgyApiIntegrationTests {
     
     @Test
     public void testLiturgApproxDate(){
-    	get("/api/liturgy/?filter[approxDate]=2017-06-03").
+    	given(this.spec)
+   // 	.accept("application/json") 
+		.filter(document("approxDate"))
+		.get("/api/liturgy/?filter[approxDate]=2017-06-03").
     	then()
     	.statusCode(200)
     	.body("data", hasSize(1))
@@ -68,7 +95,10 @@ public class LiturgyApiIntegrationTests {
     
     @Test
     public void testLitugDateRange(){
-    	get("/api/liturgy/?filter[startDate]=2017-01-01&filter[endDate]=2018-01-01").
+    	given(this.spec)
+  //  	.accept("application/json") 
+		.filter(document("getRange"))
+		.get("/api/liturgy/?filter[startDate]=2017-01-01&filter[endDate]=2018-01-01").
     	then()
     	.statusCode(200)
     	.body("data", hasSize(68));
@@ -76,7 +106,10 @@ public class LiturgyApiIntegrationTests {
     
     @Test
     public void testLiturgHolidayAndYear(){ 	
-    	get("/api/liturgy/?filter[holiday]=Christmas Day&filter[year]=2018")
+    	given(this.spec)
+   // 	.accept("application/json") 
+		.filter(document("getHolidayAndYear"))
+		.get("/api/liturgy/?filter[holiday]=Christmas Day&filter[year]=2018")
     	.then()
     	.statusCode(200)
     	.body("data", hasSize(1))
@@ -85,7 +118,10 @@ public class LiturgyApiIntegrationTests {
     
     @Test
     public void testLitugYear(){
-    	get("/api/liturgy/?filter[year]=2017")
+    	given(this.spec)
+   // 	.accept("application/json") 
+		.filter(document("getYear"))
+		.get("/api/liturgy/?filter[year]=2017")
     	.then()
     	.statusCode(200)
     	.body("data", hasSize(71));
@@ -95,7 +131,10 @@ public class LiturgyApiIntegrationTests {
     public void testLiturgHoliday(){
     	int year = (new DateTime()).getYear();
     	
-    	get("/api/liturgy/?filter[holiday]=Christmas Day")
+    	given(this.spec)
+    //	.accept("application/json") 
+		.filter(document("getHoliday"))
+		.get("/api/liturgy/?filter[holiday]=Christmas Day")
     	.then()
     	.statusCode(200)
     	.body("data", hasSize(1))
@@ -104,7 +143,10 @@ public class LiturgyApiIntegrationTests {
     
     @Test
     public void testDate_error(){
-    	get("/api/liturgy/?filter[date]=asdfasdf")
+    	given(this.spec)
+    //	.accept("application/json") 
+		.filter(document("error"))
+		.get("/api/liturgy/?filter[date]=asdfasdf")
     	.then()
     	.statusCode(500);
     }
